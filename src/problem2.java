74627052a57bd302d79ae5bdb76ba8230273f1b1
unknown <hchen@HCHEN-T430.jnpr.net>
2014-11-22 20:34:03 -0800

    my problem 2

--- src/problem2.java
+++ src/problem2.java
@@ -3,7 +3,24 @@ public class problem2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int f1;
+		int f2;
+		int f3;
+		int sum = 0;
+		
+		f1 = 1;
+		f2 = 2;
+		f3 = f1 + f2;
+		while (f1 <= 4000000) {
+//			System.out.println(f1);
+			if (f1 % 2 == 0) {
+				sum += f1;
+			}
+			f3 = f1 + f2;
+			f1 = f2;
+			f2 = f3;
+		}
+		System.out.println(sum);
 	}
 
 }
